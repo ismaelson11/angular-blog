a960f18d34249650f3a245f77e8d9c8de61fe10f
ismaelson11 <ismaelson111@gmail.com>
2023-10-19 21:27:03 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,27 @@
+export const dataFake = [
+    {
+        "id": '1',
+        "title": "Adulto Ney foi de base",
+        "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni fuga modi accusantium amet debitis voluptate nulla commodi! Recusandae eum reprehenderit labore sed, aut reiciendis atque nihil, mollitia delectus aperiam tenetur?",
+        "photoCover": "https://s2-ge.glbimg.com/CJA6C0jTTS9FtWEaTNgXxH9vt7U=/49x712:2743x2226/810x456/smart/filters:max_age(3600)/https://i.s3.glbimg.com/v1/AUTH_bc8228b6673f488aa253bbcb03c80ec5/internal_photos/bs/2023/E/3/62gqZpQxOfPsXtGWvf6A/2023-10-18t012640z-23012393-up1ejai040di0-rtrmadp-3-soccer-worldcup-ury-bra-report.jpg"
+    },
+    {
+        "id": '2',
+        "title": "Melhor visitante da Série C, Amazonas embarca a SC rumo ao título",
+        "description": "Esperança de título leva público amazonense a loucura",
+        "photoCover": "https://s2-ge.glbimg.com/n4HsyjKeEDLK8wWf39uXCPvIgQc=/810x456/top/smart/https://i.s3.glbimg.com/v1/AUTH_bc8228b6673f488aa253bbcb03c80ec5/internal_photos/bs/2023/1/j/mIty11SZ2PCeMpUCVL7A/f560be3a-c952-443c-8f2e-84c084993637.jfif"
+    },
+    {
+        "id": '3',
+        "title": "Empate na Arena da Amazonia deixou fãs apreensívos para próximo jogo",
+        "description": "Amazonas e Brusque ficam somente no empate e decisão segue para SC",
+        "photoCover": "https://s2-ge.glbimg.com/tYYUPbepJLNzJpQ6AGh0KTDIbEg=/0x746:3197x2544/810x456/smart/filters:max_age(3600)/https://i.s3.glbimg.com/v1/AUTH_bc8228b6673f488aa253bbcb03c80ec5/internal_photos/bs/2023/m/U/vwUPueR1id7kUKmMWkNQ/amazonas-x-brusque-final-serie-c-jogo-de-ida1.jpg"
+        
+    },
+    {
+        "id": '4',
+        "title": "Escalações, desfalques e arbitragem. ACOMPANHE",
+        "description": "Deixe seu palpite sobre quem serão os 11 em campo em busca de fechar com chave de ouro a super-campanha do Amazonas rumo ao título da série C",
+        "photoCover": "https://s2-ge.glbimg.com/jZi_2BOOR3FM5TbT-_lVh0bxPzI=/810x456/top/smart/https://i.s3.glbimg.com/v1/AUTH_bc8228b6673f488aa253bbcb03c80ec5/internal_photos/bs/2023/y/R/KR6UigRBuTJsnu5oIWJQ/arena-2.jfif"
+    }
+]
